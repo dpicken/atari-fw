0cfb637588a1db24fd5e06dd16f5b29500bdd37b
Daniel Picken <dpicken@users.noreply.github.com>
2024-10-09 22:29:03 -0700

    SD SPI fixes/improvements

--- src/sd/Controller.cc
+++ src/sd/Controller.cc
@@ -1,7 +1,6 @@
 #include "Controller.h"
 
 #include "Traits.h"
-#include "CSD.h"
 
 #include "fs/automount/Manager.h"
 
@@ -43,15 +42,15 @@ void sd::Controller::pollCardInserted() {
   m_power.activate();
 
   if (!initialize()) {
+    m_currentCard = Card::makeInvalid(this, m_initializationFailureReason);
+    ::fs::automount::Manager::instance()->onBlockDeviceAvailable(m_currentCard, m_currentCard->name());
     return;
   }
 
-  // Get card size.
-  if (!m_accessor.sendCsd().isSuccess()) {
-    return;
-  }
   CSD csd;
-  if (!m_accessor.rx(&csd)) {
+  if (!m_accessor.readCsd(&csd)) {
+    m_currentCard = Card::makeInvalid(this, "read-csd");
+    ::fs::automount::Manager::instance()->onBlockDeviceAvailable(m_currentCard, m_currentCard->name());
     return;
   }
 
@@ -82,6 +81,8 @@ void sd::Controller::pollCardEjected() {
 }
 
 bool sd::Controller::initialize() {
+  m_initializationFailureReason = "";
+
   // Set initialization frequency.
   m_accessor.setBaudRate(Traits::spi_init_baud_rate);
 
@@ -95,6 +96,7 @@ bool sd::Controller::initialize() {
   // Part1_Physical_Layer_Simplified_Specification_Ver8.00.pdf, 7.2.1:
 
   if (!m_accessor.goIdleState().isInIdleStateOnly()) {
+    m_initializationFailureReason = "go-idle-state";
     return false;
   }
 
@@ -104,12 +106,15 @@ bool sd::Controller::initialize() {
   auto ifcResponse = m_accessor.sendIfCond(vhs, checkPattern);
   if (ifcResponse.isInIdleStateOnly()) {
     if (!ifcResponse.isVHS(vhs)) {
+      m_initializationFailureReason = "send-if-cond:vhs";
       return false;
     }
     if (!ifcResponse.isCheckPattern(checkPattern)) {
+      m_initializationFailureReason = "send-if-cond:check-pattern";
       return false;
     }
   } else if (!ifcResponse.isIllegalCommand()) {
+    m_initializationFailureReason = "send-if-cond:not-illegal_command";
     return false;
   }
 
@@ -117,30 +122,35 @@ bool sd::Controller::initialize() {
 
   auto ocrResponse = m_accessor.readOcr();
   if (!ocrResponse.isInIdleStateOnly()) {
+    m_initializationFailureReason = "read-ocr";
     return false;
   }
   if (!ocrResponse.is3V3Supported()) {
+    m_initializationFailureReason = "read-ocr:3v3-not-supported";
     return false;
   }
 
   Response::R1 socResponse;
-  for (unsigned int retry = 0; retry != Traits::rx_retry_count; ++retry) {
+  for (unsigned int retry = 0; retry != Traits::command_retry_count; ++retry) {
     socResponse = m_accessor.sdSendOpCond(sdscSupportOnly);
     if (socResponse.isSuccess() || !socResponse.isInIdleStateOnly()) {
       break;
     }
-    m_busyWait(Traits::rx_retry_delay);
+    m_busyWait(Traits::command_retry_delay);
   }
   if (!socResponse.isSuccess()) {
+    m_initializationFailureReason = "send-op-cond";
     return false;
   }
 
   ocrResponse = m_accessor.readOcr();
   if (!ocrResponse.isSuccess()) {
+    m_initializationFailureReason = "read-ocr";
     return false;
   }
 
   if (!ocrResponse.isPowerUpComplete()) {
+    m_initializationFailureReason = "power-up-incomplete";
     return false;
   }
 
@@ -150,6 +160,7 @@ bool sd::Controller::initialize() {
 
   if (m_isSDSC) {
     if (!m_accessor.setBlocklen(BlockSize::byte_count).isSuccess()) {
+      m_initializationFailureReason = "set-blocklen";
       return false;
     }
   }
@@ -189,13 +200,9 @@ bool sd::Controller::cacheBlock(std::uint64_t byteOffset) {
     return true;
   }
 
-  std::uint32_t address = static_cast<std::uint32_t>(m_isSDSC ? BlockSize::blockToByteOffset(blockAddress) : blockAddress);
-  if (!m_accessor.readSingleBlock(address).isSuccess()) {
-    return false;
-  }
-
   m_cachedBlockAddress = std::nullopt;
-  if (!m_accessor.rx(m_cachedBlock.data(), m_cachedBlock.size())) {
+  std::uint32_t address = static_cast<std::uint32_t>(m_isSDSC ? BlockSize::blockToByteOffset(blockAddress) : blockAddress);
+  if (!m_accessor.readBlock(&m_cachedBlock, address)) {
     return false;
   }
   m_cachedBlockAddress = blockAddress;
