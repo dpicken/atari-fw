86d6de7148762dc973020e14eae7f2496ed9b41f
Daniel Picken <dpicken@users.noreply.github.com>
2024-10-09 22:29:03 -0700

    SD SPI fixes/improvements

---
+++ src/sd/Block.h
@@ -0,0 +1,15 @@
+#ifndef sd_Block_h
+#define sd_Block_h
+
+#include "BlockSize.h"
+
+#include <array>
+#include <cstdint>
+
+namespace sd {
+
+using Block = std::array<std::uint8_t, BlockSize::byte_count>;
+
+} // namespace sd
+
+#endif // ifndef sd_Block_h
